81a74cfa5dabb1f7d28009fab324a6c5ad1fe753
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-12-01 16:14:34 +0100

    Created a function where i put number inside array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 // Punto 1 readme.md
-// Creo la funzione che mi genera 5 numeri casuali da 1 a 100
-function createNumbers(){
+// Creo la funzione che mi genera numeri casuali da 1 a 100
+function createNumbers(num){
     let number = Math.floor(Math.random() * 100 + 1);
 
     return number;
 }
 
+// Punto 2 readme.md
+// Inserisco i numeri casuali dentro un array;
+function arrayNumbers(){
+    let array = [];
+    
+    for(let i=0; i<5; i++){
+        array.push(createNumbers(array));
+    }
+
+    console.log(array);
+    return array
+}
+
+arrayNumbers();
