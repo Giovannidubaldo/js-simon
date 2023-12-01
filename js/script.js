95d797d1f945d856e361ff00aa825f64008daa2e
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-12-01 16:05:15 +0100

    Created the function that generates random numbers

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Punto 1 readme.md
+// Creo la funzione che mi genera 5 numeri casuali da 1 a 100
+function createNumbers(){
+    let number = Math.floor(Math.random() * 100 + 1);
+
+    return number;
+}
+
